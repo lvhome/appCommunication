8930f179dfbb607dad267a0ebc98c109efad4ced
lvhome <1353462015@qq.com>
2018-10-23 08:13:07 +0800

    APP通讯

---
+++ ProjectDemo/ProjectDemo/ViewController.m
@@ -0,0 +1,52 @@
+//
+//  ViewController.m
+//  ProjectDemo
+//
+//  Created by 吕洪建 on 2018/10/23.
+//  Copyright © 2018年 吕洪建. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(100, 200, 300, 200);
+    btn.backgroundColor = [UIColor redColor];
+    [self.view addSubview:btn];
+    [btn addTarget:self action:@selector(onClick) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)onClick {
+    NSLog(@"点击事件");
+    //之前配置的白名单，就是需要跳转对方App的key，即对方设置的url
+    NSString * UrlStr = @"ProjectTwoDemo://index?a=b&b=c";
+    //如果想传递参数 只要在index后面配置即可 ，同理打开其他的web一样传值
+    NSURL * url = [NSURL URLWithString:UrlStr];
+    // 在这里可以先做个判断
+    if ([[UIApplication sharedApplication]canOpenURL:url]) {
+        if (@available(iOS 10.0, *)) {//iOS 10之后
+            [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+        } else {
+            [[UIApplication sharedApplication] openURL:url];
+        }
+    }else{
+        NSLog(@"尚未安装该应用");
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ProjectDemo/ProjectDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProjectDemo
+//
+//  Created by 吕洪建 on 2018/10/23.
+//  Copyright © 2018年 吕洪建. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
